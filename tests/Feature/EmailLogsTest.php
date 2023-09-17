0aad75fe554fc4e9ba0f5f5abb2b426a3671b7ab
Victor Briand <victor.briand78@gmail.com>
2023-09-17 12:53:23 +0100

    Pest test created

---
+++ tests/Feature/EmailLogsTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Mail\TestMailable;
+use Illuminate\Mail\Events\MessageSent;
+use Illuminate\Mail\SentMessage;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
+use Symfony\Component\Mailer\Envelope;
+use Symfony\Component\Mailer\SentMessage as MailerSentMessage;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Mime\RawMessage;
+use Vic\EmailLogger\Listeners\EmailSentListener;
+
+it('should log every outgoing email', function () {
+    Mail::fake();
+    Event::fake();
+
+    Log::spy();
+
+    (new EmailSentListener())->handle(new MessageSent(
+        new SentMessage(
+            new MailerSentMessage(
+                new RawMessage('test'),
+                new Envelope(
+                    new Address('sender@example.com'),
+                    [
+                        new Address('recipient1@example.com'),
+                        new Address('recipient2@example.com')
+                    ]
+                )
+            )
+        )
+    ));
+
+    Log::shouldHaveReceived('info')
+        ->once()
+        ->with('Email Sent');
+});
